c5dc64272ff7be55842aa27fcb75ba4c248c49d9
priti-swn <pritivash.swain.1234@gmail.com>
2019-06-07 19:46:41 +0530

    first commit

---
+++ HealthManage.py
@@ -0,0 +1,99 @@
+# Health Management System
+# 3 clients - Pritivash, Raman and Naman
+
+def getdate():
+    import datetime
+    return datetime.datetime.now()
+
+# Total 6 files
+# write a function that when executed takes as input client name
+# One more function to retrieve exercise or food for any client
+
+
+import datetime
+def gettime():
+    return datetime.datetime.now()
+
+def take(k):
+    if k==1:
+        c=int(input("enter 1 for excersise and 2 for food"))
+        if(c==1):
+            value=input("type here\n")
+            with open("Pritivash-ex.txt","a") as op:
+                op.write(str([str(gettime())])+": "+value+"\n")
+            print("successfully written")
+        elif(c==2):
+            value = input("type here\n")
+            with open("Pritivash-food.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+    elif(k==2):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            value = input("type here\n")
+            with open("Raman-ex.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+        elif (c == 2):
+            value = input("type here\n")
+            with open("Raman-food.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+    elif(k==3):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            value = input("type here\n")
+            with open("Naman-ex.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+        elif (c == 2):
+            value = input("type here\n")
+            with open("Naman-food.txt", "a") as op:
+                op.write(str([str(gettime())]) + ": " + value + "\n")
+            print("successfully written")
+    else:
+        print("plz enter valid input (1(harry),2(rohan),3(hammad)")
+def retrieve(k):
+    if k==1:
+        c=int(input("enter 1 for excersise and 2 for food"))
+        if(c==1):
+            with open("Pritivash-ex.txt") as op:
+                for i in op:
+                    print(i,end="")
+        elif(c==2):
+            with open("Pritivash-food.txt") as op:
+                for i in op:
+                    print(i, end="")
+    elif(k==2):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            with open("Raman-ex.txt") as op:
+                for i in op:
+                    print(i, end="")
+        elif (c == 2):
+            with open("Raman-food.txt") as op:
+                for i in op:
+                    print(i, end="")
+    elif(k==3):
+        c = int(input("enter 1 for excersise and 2 for food"))
+        if (c == 1):
+            with open("Naman-ex.txt") as op:
+                for i in op:
+                    print(i, end="")
+        elif (c == 2):
+            with open("Naman-food.txt") as op:
+                for i in op:
+                    print(i, end="")
+    else:
+        print("plz enter valid input (Pritivash, Raman and Naman)")
+print("health management system: ")
+a=int(input("Press 1 for log the value and 2 for retrieve "))
+
+if a==1:
+    b = int(input("Press 1 for Pritivash 2 for Raman 3 for Naman "))
+    take(b)
+else:
+    b = int(input("Press 1 for Pritivash 2 for Raman 3 for Naman "))
+    retrieve(b)
+  
+    
